87a633953d7dcd61c61d065cdaa1a6fcd3454626
Anshul Gupta <anshul.gup96@gmail.com>
2020-02-07 19:36:12 +0530

    Before trying theme

--- static/page-js.js
+++ static/page-js.js
@@ -1,38 +1,7 @@
-
-var obj = {tasks: ['get milk', 'drink milk'], deadlines: ['20/10/2010', '10/11/2020']};
-
-
 $(document).ready(function(){
-    for(i = 0; i < obj.tasks.length; i++){
-        code = "<li class='list-group-item task'><input type='checkbox' class='task-done'> " + obj.tasks[i] + " <span class='date'>Deadline: " + obj.deadlines[i] + "</span></li>";
-        $('#task-section').append(code);
-    }
-    
-    $('.task-done').click(function(){
-        $(this).parent().hide();
-    });
-
-    $('#new-task-button').click(function(){
-        $('#new-task-input').toggle();
-    });
-    
-    
-    //to get data from form submissions
-    
-    $('#new-task-input').submit(function(){
-        var formData = new FormData($(this)[0]); 
-        console.log(formData);
-    });
-    
-
-    var num = obj.tasks.length;
-    $('#my-tasks').append(' (' + num + ')');
+   $('[data-toggle="offcanvas"]').click(function(){
+       $("#navigation").toggleClass("hidden-xs");
+   });
 });
 
 
-
-//var obj = {'first name' : "Sammy", "last name" : "Shark", "location" : "Ocean"};
-
-//for(var property in obj){
-//    console.log(property + ': ' + obj[property]);
-//}

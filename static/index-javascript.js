515c12d15b80a1a982cc1986f8d7c26d00e73628
Anshul Gupta <anshul.gup96@gmail.com>
2020-02-09 14:10:35 +0530

    Before changing dashboard

--- static/index-javascript.js
+++ static/index-javascript.js
@@ -1,13 +1,63 @@
-function validate (input) {
-    if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-        if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-            return false;
-        }
-    }
-    else {
-        if($(input).val().trim() == ''){
-            return false;
-        }
-    }
-}
+$(document).ready(function(){
+    
+    $("#signup-button").click(function(){
+        var new_user = {
+            "username": $('#signup-email').val(),
+            "password": $('#signup-password').val(),
+        };
+        
+        $.ajax({
+            type: 'GET',
+            url: 'https://strum-task-manager.herokuapp.com/api/users',
+            success: function(data){
+                console.log(data);
+            }
+        });
+        return false;
+        
+    });
+        /*
+        console.log('hello world');
+        var new_user = {
+            "username": $('#signup-email').val(),
+            "password": $('#signup-password').val(),
+        };
 
+        $.ajax({
+            type: 'POST',
+            url: 'text.txt',
+            data: new_user,
+            success: function(data){
+                console.log(data);
+            }
+        });
+        
+    });
+    
+*/
+});
+
+
+
+
+//.ajax to send a request
+
+//.each(data, function(i, order)) to run a function for each item in some data
+
+
+
+/*
+$.ajax({
+  type: "POST",
+  beforeSend: function(request) {
+    request.setRequestHeader("Authority", authorizationToken);
+  },
+  url: "entities",
+  data: "json=" + escape(JSON.stringify(createRequestObject)),
+  processData: false,
+  success: function(msg) {
+    $("#results").append("The result =" + StringifyPretty(msg));
+  }
+});
+
+*/
--- static/page-js.js
+++ static/page-js.js
@@ -1,7 +1,38 @@
+var obj = {tasks: ['get milk', 'drink milk'], deadlines: ['20/10/2010', '10/11/2020']};
+
+
 $(document).ready(function(){
-   $('[data-toggle="offcanvas"]').click(function(){
-       $("#navigation").toggleClass("hidden-xs");
-   });
+    for(i = 0; i < obj.tasks.length; i++){
+        code = "<li class='list-group-item task'><input type='checkbox' class='task-done'> " + obj.tasks[i] + " <span class='date'>Deadline: " + obj.deadlines[i] + "</span></li>";
+        $('#task-section').append(code);
+    }
+
+    
+    $('.task-done').click(function(){
+        $(this).parent().fadeOut("slow");
+    });
+
+    $('#new-task-button').click(function(){
+        $('#new-task-input').toggle();
+    });
+    
+    $('#new-team-button').click(function(){
+        $('#new-team-input').toggle();
+    });
+
+    //to get data from form submissions
+
+    $('#new-task-input').submit(function(){
+        var formData = new FormData($(this)[0]); 
+        console.log(formData);
+    });
+
+
+    var num = obj.tasks.length;
+    $('#my-tasks').append(' (' + num + ')');
+    
+    $('input').click(function(){
+        return false;
+    });
+    
 });
-
-
